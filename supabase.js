1310e40b19f8905d85467700b0698f1bc1ea5023
selamtesfaye2x-cmd <selamtesfaye2x@gmail.com>
2026-08-27 13:18:49 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://hbuoucmdgtwryahlnjzf.supabase.co";
+const SUPABASE_KEY = "sb_publishable_OqqklL691qHVkDvkZisQ5w_IhepZwCb";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
