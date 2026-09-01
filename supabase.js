87cdd0d3c14e54d38e92990e0cafda2c95c7e0ef
selamtesfaye2x-cmd <selamtesfaye2x@gmail.com>
2026-09-01 18:36:31 +0300

    Fix formatting in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,8 @@
 const SUPABASE_URL = "https://hbuoucmdgtwryahlnjzf.supabase.co";
+
 const SUPABASE_KEY = "sb_publishable_OqqklL691qHVkDvkZisQ5w_IhepZwCb";
 
 const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
+SUPABASE_URL,
+SUPABASE_KEY
 );
